75ac05c9d9e89683ddb01895fd9b2d063a8a675f
Stuffbutts <stuffbruss@gmail.com>
2017-08-20 17:24:18 -0400

    major tile improvements. Put sprites into an actual sprite sheet. Added support for pixels per unit value on sprite importer. Added grid for gameworld. Made grid update on changes to map size.

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -11,9 +11,18 @@ public class TileMapEditor : Editor {
     public override void OnInspectorGUI()
     {
         EditorGUILayout.BeginVertical();
+        // Used to update grid on change
+        var oldSize = map.mapSize;
+    
         // Reads value from map and returns V2 field with values
         // refreshes on value change
         map.mapSize = EditorGUILayout.Vector2Field("Map Size:", map.mapSize);
+
+        if(map.mapSize != oldSize)
+        {
+            UpdateCalculations();
+        }
+
         // Enable texture2D field (allowing sprite textures)
         map.texture2D = (Texture2D)EditorGUILayout.ObjectField("Texture 2d:", map.texture2D, typeof(Texture2D), false);
 
@@ -24,6 +33,9 @@ public class TileMapEditor : Editor {
         } else
         {
             EditorGUILayout.LabelField("Tile Size:", map.tileSize.x + "x" + map.tileSize.y);
+            EditorGUILayout.LabelField("Grid Size in Units:", map.gridSize.x + "x" + map.gridSize.y);
+            EditorGUILayout.LabelField("Pixels to Units:", map.pixelsToUnits.ToString());
+
         }
 
         EditorGUILayout.EndVertical();
@@ -33,7 +45,40 @@ public class TileMapEditor : Editor {
     {
         // Link component values to TileMap
         map = target as TileMap;
+
         // Auto select view tool
         Tools.current = Tool.View;
+
+        // Read all sprites attached to a Texture2D when we select a TileMap
+        // Test for existence of texture2D in map
+        if(map.texture2D != null)
+        {
+            UpdateCalculations();   
+        }
+    }
+
+    void UpdateCalculations()
+    {
+        // Get the asset path of the reference Texture2D
+        var path = AssetDatabase.GetAssetPath(map.texture2D);
+        // Texture should be split by grid, load all sprites in the texture into memory
+        map.spriteReferences = AssetDatabase.LoadAllAssetsAtPath(path);
+
+        // In order to determine tile size we need to reference the first sprite in the references
+        // NOTE: the first index in the sprite references is the texture itself, so we select the second index which is the first sliced sprite
+        var sprite = (Sprite)map.spriteReferences[1];
+        // Grab width and height
+        var width = sprite.textureRect.width;
+        var height = sprite.textureRect.height;
+
+        // Tell the map the tile size
+        map.tileSize = new Vector2(width, height);
+
+        // Get pixels to units
+        map.pixelsToUnits = (int)(sprite.rect.width / sprite.bounds.size.x);
+
+        // Determine grid total size
+        // Created sprite should have 
+        map.gridSize = new Vector2((width / map.pixelsToUnits) * map.mapSize.x, (height / map.pixelsToUnits) * map.mapSize.y);
     }
 }
--- Assets/Scripts/Tilemap.cs
+++ Assets/Scripts/Tilemap.cs
@@ -13,6 +13,13 @@ public class TileMap : MonoBehaviour {
     public Vector2 mapSize = new Vector2(20, 10);
     public Texture2D texture2D;
     public Vector2 tileSize = new Vector2();
+    public Vector2 gridSize = new Vector2();
+
+    // Store all sprites attached to a Texture2D
+    public Object[] spriteReferences;
+
+    // 
+    public int pixelsToUnits = 100;
 
     // Use this for initialization
     void Start () {
@@ -23,4 +30,43 @@ public class TileMap : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnDrawGizmosSelected()
+    {
+        var pos = transform.position;
+
+        if(texture2D != null)
+        {
+            // Draw grid for tiling
+            Gizmos.color = Color.gray;
+            var row = 0;
+            var maxColumns = mapSize.x;
+            var total = mapSize.x * mapSize.y;
+            var tile = new Vector3(tileSize.x / pixelsToUnits, tileSize.y / pixelsToUnits);
+            var offset = new Vector2(tile.x / 2, tile.y / 2);
+
+            for(var i = 0; i < total; i++)
+            {
+                var column = i % maxColumns;
+
+                var newX = (column * tile.x) + offset.x + pos.x;
+                var newY = -(row * tile.y) - offset.y + pos.y;
+
+                Gizmos.DrawWireCube(new Vector2(newX, newY), tile);
+
+                if(column == maxColumns - 1)
+                {
+                    row++;
+                }
+            }
+
+            // Give gizmo white border
+            Gizmos.color = Color.white;
+            // Gizmos are drawn from center, so we need the center point
+            var centerX = pos.x + (gridSize.x / 2);
+            var centerY = pos.y - (gridSize.y / 2);
+
+            Gizmos.DrawWireCube(new Vector2(centerX, centerY), gridSize);
+        }
+    }
 }
